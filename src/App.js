94bea00f3f759709cc7fb3eb660f76c9f5a529ef
Rakshith hegde <rakshithhgd@gmail.com>
2021-06-12 18:40:07 +0530

    instagram

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="app__header">
+            <img 
+              className="app__headerimage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png"
+              alt="" />
+            <h1>Instagram 🔗 !</h1>
+        </div>
+        
+
+        {/*header */}
+
+
+        {/* posts*/}
+        {/* posts*/}
     </div>
   );
 }
